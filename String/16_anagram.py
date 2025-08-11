7fc7bf1428c3b5024bc8ac205d242908497c71e2
Shreyash Waralkar <waralkarshayu@gmail.com>
2025-08-11 20:21:05 +0530

    16 commit

---
+++ String/16_anagram.py
@@ -0,0 +1,32 @@
+# 16. Check if two strings are anagram of each other
+
+def anagram(str1,str2):
+    if len(str1)!=len(str2):
+        return False
+    
+    for ele in str1:
+        count1 = 0
+        count2 = 0
+
+        for ch in str1:
+            if ch==ele:
+                count1+=1
+        
+        for ch in str2:
+            if ch==ele:
+                count2+=1
+        
+        if count1!=count2:
+            return False
+    
+    return True
+        
+str1 = 'soon'
+str2 = 'nose'
+
+ans = anagram(str1,str2)
+
+if ans==True:
+    print("Anagram")
+else:
+    print("Not Anagram")
